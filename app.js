06dfb762b535091abc4a69013063af73376ff765
janzibve-dot <janzibve@gmail.com>
2026-01-10 19:09:26 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+// Логика твоего проекта TEAMS WAR
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Портал TEAMS WAR от NiksOnGame запущен!");
+    
+    // Если ты добавлял переводы или уведомления, код должен быть здесь
+    // Пока оставим этот проверочный код
+});
